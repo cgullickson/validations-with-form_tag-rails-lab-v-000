06790c350857ca29a8a959734356820631315b2d
Chris Gullickson <cgullickson5@gmail.com>
2018-02-08 00:54:31 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,14 @@
+
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
 
   def new
+    @author = Author.new
+  end
+
+  def create
     @author = Author.new(author_params)
     if @author.valid?
       @author.save
@@ -11,23 +16,6 @@ class AuthorsController < ApplicationController
     else
       render :new
     end
-  end
-
-  def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
-  end
-
-  def edit
-    @author = Author.find(params[:id])
-  end
-
-  def update
-    @author = Author.find(params[:id])
-    @author.update(author_params)
-    @author.save
-    redirect_to author_path(@author)
   end
 
   private
